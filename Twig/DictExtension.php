9f022d0a0da9facd028db1dadab5f7830c24f34c
yangshuai <yangshuai@howzhi.com>
2017-05-09 17:27:12 +0800

    dict locale

--- Twig/DictExtension.php
+++ Twig/DictExtension.php
@@ -5,11 +5,12 @@ class DictExtension extends \Twig_Extension
 {
     protected $collector;
     protected $locale;
+    protected $container;
 
     public function __construct($collector,$container)
     {
         $this->collector = $collector;
-        $this->locale = $container->get('request')->getLocale();
+        $this->container = $container;
     }
 
     public function getFunctions()
@@ -22,16 +23,26 @@ class DictExtension extends \Twig_Extension
 
     public function getDict($name)
     {
+        $this->getLocale();
         return $this->collector->getDictMap($this->locale,$name);
     }
 
     public function getDictText($name, $key, $default = '')
     {
+        $this->getLocale();
         return $this->collector->getDictText($this->locale,$name, $key, $default);
     }
 
     public function getName()
     {
         return 'codeages_plugin_dict_extension';
+    }
+
+    private function getLocale()
+    {
+        if($this->locale){
+            return ;
+        }
+        $this->locale = $this->container->get('request')->getLocale();
     }
 }
